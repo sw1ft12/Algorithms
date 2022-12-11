f8c9e56b13e8ef66fb489f0db4a2c21d4b2e27ff
Daniil <99120105+sw1ft12@users.noreply.github.com>
2022-12-12 00:51:30 +0300

    Update taskC.cpp

--- contest1/taskC.cpp
+++ contest1/taskC.cpp
@@ -3,33 +3,37 @@
 #include <queue>
 
 
-const int alph_sz = 26;
+const int ALPH_SZ = 26;
 
 
-struct Node{
+struct Node {
     std::vector<int> to;
     std::vector<int> go;
     int link;
     int length = 0;
     int pos = -1;
     bool term = false;
-    Node() : to(std::vector<int>(alph_sz, -1)), go(std::vector<int>(alph_sz)), link(-1) {}
+    Node() : to(std::vector<int>(ALPH_SZ, -1)), go(std::vector<int>(ALPH_SZ)), link(-1) {}
 };
 
+char getSymbol() {
+    return 'a';
+}
 
-void add(const std::string& s, std::vector<Node>& trie, int num, std::vector<int> &pos){
-    if(trie.empty()){
+void add(const std::string& s, std::vector<Node>& trie, int num, std::vector<int>& pos) {
+    if (trie.empty()) {
         trie.emplace_back();
     }
     int v = 0;
-    for(size_t i = 0; i < s.size(); ++i){
-        if(trie[v].to[s[i] - 'a'] == -1){
-            trie[v].to[s[i] - 'a'] = trie.size();
+    char sym = getSymbol();
+    for (size_t i = 0; i < s.size(); ++i) {
+        if (trie[v].to[s[i] - sym] == -1) {
+            trie[v].to[s[i] - sym] = trie.size();
             trie.emplace_back();
         }
-        v = trie[v].to[s[i] - 'a'];
+        v = trie[v].to[s[i] - sym];
     }
-    if(trie[v].term){
+    if (trie[v].term) {
         pos.push_back(trie[v].pos);
         return;
     }
@@ -39,29 +43,30 @@ void add(const std::string& s, std::vector<Node>& trie, int num, std::vector<int
     trie[v].pos = num;
 }
 
-void Aho_Corasick(std::vector<Node> &trie){
+void Aho_Corasick(std::vector<Node>& trie) {
     trie[0].link = 0;
-    for(size_t i = 0; i < alph_sz; ++i){
-        if(trie[0].to[i] != -1){
+    for (size_t i = 0; i < ALPH_SZ; ++i) {
+        if (trie[0].to[i] != -1) {
             trie[0].go[i] = trie[0].to[i];
         }
-        else{
+        else {
             trie[0].go[i] = 0;
         }
     }
     std::queue<int> q;
     q.push(0);
-    while(!q.empty()){
+    while (!q.empty()) {
         int v = q.front();
         q.pop();
-        for(size_t i = 0; i < alph_sz; ++i){
+        for (size_t i = 0; i < ALPH_SZ; ++i) {
             int u = trie[v].to[i];
-            if(u != -1) {
+            if (u != -1) {
                 trie[u].link = (v == 0 ? 0 : trie[trie[v].link].go[i]);
-                for (size_t j = 0; j < alph_sz; ++j) {
+                for (size_t j = 0; j < ALPH_SZ; ++j) {
                     if (trie[u].to[j] != -1) {
                         trie[u].go[j] = trie[u].to[j];
-                    } else {
+                    }
+                    else {
                         trie[u].go[j] = trie[trie[u].link].go[j];
                     }
                 }
@@ -71,8 +76,24 @@ void Aho_Corasick(std::vector<Node> &trie){
     }
 }
 
+std::vector<std::vector<int>> getAns(const std::vector<Node>& trie, const std::string &s, int n) {
+    std::vector<std::vector<int>> ans(n);
+    int v = 0;
+    char sym = getSymbol();
+    for (size_t i = 0; i < s.size(); ++i) {
+        v = trie[v].go[s[i] - sym];
+        int u = v;
+        while (u != 0) {
+            if (trie[u].term) {
+                ans[trie[u].pos].push_back(static_cast<int>(i) - trie[u].length + 2);
+            }
+            u = trie[u].link;
+        }
+    }
+    return ans;
+}
 
-int main(){
+int main() {
     std::ios_base::sync_with_stdio(false);
     std::cout.tie(0);
     std::cin.tie(0);
@@ -82,33 +103,24 @@ int main(){
     std::cin >> n;
     std::vector<Node> trie;
     std::vector<int> pos;
-    for(int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         std::string in;
         std::cin >> in;
         add(in, trie, i, pos);
     }
     Aho_Corasick(trie);
     int v = 0;
-    std::vector<std::vector<int>> ans(n);
-    for(size_t i = 0; i < s.size(); ++i){
-        v = trie[v].go[s[i] - 'a'];
-        int u = v;
-        while(u != 0){
-            if(trie[u].term) {
-                ans[trie[u].pos].push_back(static_cast<int>(i) - trie[u].length + 2);
-            }
-            u = trie[u].link;
-        }
-    }
-    for(auto j : pos){
-        if(!ans[j].empty()) {
+    std::vector<std::vector<int>> ans = getAns(trie, s, n);
+    
+    for (auto j : pos) {
+        if (!ans[j].empty()) {
             std::cout << ans[j].size() << ' ';
             for (auto i : ans[j]) {
                 std::cout << i << ' ';
             }
             std::cout << '\n';
         }
-        else{
+        else {
             std::cout << 0 << '\n';
         }
     }
