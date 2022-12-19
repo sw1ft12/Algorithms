c4a4d2bd25a81b35587bd7ee8e0140a0c6398f20
Daniil <99120105+sw1ft12@users.noreply.github.com>
2022-12-19 23:25:23 +0300

    Update taskL.cpp

--- contest3/taskL.cpp
+++ contest3/taskL.cpp
@@ -1,104 +1,130 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
-const long long N = 100005;
 
 struct Point {
-    long long x, y;
-    Point(long long x = 0, long long y = 0) :x(x), y(y) {}
-    bool operator < (const Point& b) const {
-        if (y != b.y) return y < b.y;
-        return x < b.x;
+    long long x = 0, y = 0;
+    Point() = default;
+    Point(long long x, long long y) :x(x), y(y) {}
+    bool operator<(Point other) const {
+        if (y != other.y) return y < other.y;
+        return x < other.x;
     }
-    Point operator + (const Point& b) {
-        return Point(x + b.x, y + b.y);
+    Point operator+(Point other) {
+        return Point(x + other.x, y + other.y);
     }
-    Point operator - (const Point& b) {
-        return Point(x - b.x, y - b.y);
+    Point operator-(Point other) {
+        return Point(x - other.x, y - other.y);
     }
 };
 
-int n;
-int sz[N], d[3];
-Point points[3][N], v[3][N], a[N * 3], b[N * 3], c;
-
-int g(const Point& a) {
-    return a.y > 0 || (a.y == 0 && a.x >= 0);
+std::istream& operator>>(std::istream& in, Point& p) {
+    in >> p.x >> p.y;
+    return in;
 }
 
-bool cmp(const Point& a, const Point& b) {
-    int s1 = g(a), s2 = g(b);
-    if (s1 != s2) return s1 > s2;
-    return a.x * b.y - a.y * b.x > 0;
-}
+struct cmp {
+    bool operator()(Point a, Point b) {
+        bool s1 = a.y > 0 || (a.y == 0 && a.x >= 0);
+        bool s2 = b.y > 0 || (b.y == 0 && b.x >= 0);
+        if (s1 != s2) {
+            return s1 > s2;
+        }
+        return a.x * b.y - a.y * b.x > 0;
+    }
+};
 
-long long cross_prod(const Point& a, const Point& b) {
+long long cross_prod(Point a, Point b) {
     return a.x * b.y - a.y * b.x;
 }
 
-long long len(const Point& a) {
+long long dist(Point a) {
     return a.x * a.x + a.y * a.y;
 }
 
-bool func(Point v) {
-    if (v < c) return 0;
-    v = v - c;
-    int l = 0, r = n - 1;
+std::vector<Point> findMinkowskiSum(std::vector<std::vector<Point>> &polygons, Point& a) {
+    std::vector<Point> diff;
+    int cnt = 0;
+    for (size_t i = 0; i < 3; ++i) {
+        for (size_t j = 0; j < polygons[i].size() - 1; ++j) {
+            diff.emplace_back(polygons[i][j + 1] - polygons[i][j]);
+            ++cnt;
+        }
+        int index = 0;
+        for (size_t j = 1; j < polygons[i].size() - 1; ++j) {
+            if (polygons[i][j] < polygons[i][index]) {
+                index = j;
+            }
+        }
+        a = a + polygons[i][index];
+    }
+    std::sort(diff.begin(), diff.end(), cmp());
+    int k = 0;
+    for (size_t i = 0; i < cnt; ++i) {
+        int j = i + 1;
+        while (j < cnt && cross_prod(diff[i], diff[j]) == 0) {
+            ++j;
+        }
+        diff[k] = diff[i];
+        for (size_t e = i + 1; e < j; ++e) {
+            diff[k] = diff[k] + diff[e];
+        }
+        ++k;
+        i = j - 1;
+    }
+    cnt = k;
+    std::vector<Point> sum(cnt + 1);
+    for (size_t i = 1; i <= cnt; ++i) {
+        sum[i] = sum[i - 1] + diff[i - 1];
+    }
+    return sum;
+}
+
+bool isValidPoint(std::vector<Point> &sum, Point a, Point b) {
+    if (a < b) {
+        return false;
+    }
+    a = a - b;
+    int l = 0, r = sum.size() - 2;
     while (l <= r) {
         int mid = (l + r) >> 1;
-        if (cross_prod(a[mid], v) >= 0) l = mid + 1;
-        else r = mid - 1;
+        if (cross_prod(sum[mid], a) >= 0) {
+            l = mid + 1;
+        }
+        else {
+            r = mid - 1;
+        }
     }
     --l;
-    if (l == n - 1) {
-        return  cross_prod(v, a[l]) == 0 && len(v) <= len(a[l]);
+    if (l == sum.size() - 2) {
+        return  cross_prod(a, sum[l]) == 0 && dist(a) <= dist(sum[l]);
     }
-    return cross_prod(a[l + 1] - a[l], v - a[l]) >= 0;
+    return cross_prod(sum[l + 1] - sum[l], a - sum[l]) >= 0;
 }
 
 int main() {
     std::ios_base::sync_with_stdio(0);
     std::cin.tie(0);
     std::cout.tie(0);
-    n = 0;
-    c = Point(0, 0);
+    std::vector<std::vector<Point>> polygons(3);
     for (size_t i = 0; i < 3; ++i) {
-        std::cin >> sz[i];
-        for (size_t j = 0; j < sz[i]; ++j) {
-            std::cin >> points[i][j].x >> points[i][j].y;
+        int n;
+        std::cin >> n;
+        polygons[i] = std::vector<Point>(n + 1);
+        for (size_t j = 0; j < n; ++j) {
+            std::cin >> polygons[i][j];
         }
-        points[i][sz[i]] = points[i][0];
-        for (size_t j = 0; j < sz[i]; ++j) {
-            b[n++] = v[i][j] = points[i][j + 1] - points[i][j];
-        }
-        d[i] = 0;
-        for (size_t j = 1; j < sz[i]; ++j)
-            if (points[i][j] < points[i][d[i]]) {
-                d[i] = j;
-            }
-        c = c + points[i][d[i]];
+        polygons[i][n] = polygons[i][0];
     }
-    std::sort(b, b + n, cmp);
-    int k = 0;
-    for (size_t i = 0; i < n; ++i) {
-        int j;
-        for (j = i + 1; j < n && cross_prod(b[i], b[j]) == 0; ++j);
-        b[k] = b[i];
-        for (size_t e = i + 1; e < j; ++e)
-            b[k] = b[k] + b[e];
-        ++k;
-        i = j - 1;
-    }
-    n = k;
-    for (size_t i = 1; i <= n; ++i) {
-        a[i] = a[i - 1] + b[i - 1];
-    }
-    int m;
-    std::cin >> m;
-    for(size_t i = 0; i < m; ++i) {
+    Point a;
+    std::vector<Point> sum = findMinkowskiSum(polygons, a);
+    int q;
+    std::cin >> q;
+    for (size_t i = 0; i < q; ++i) {
         Point p;
         std::cin >> p.x >> p.y;
         p.x *= 3; p.y *= 3;
-        std::cout << (func(p) ? "YES" : "NO") << '\n';
+        std::cout << (isValidPoint(sum, p, a) ? "YES" : "NO") << '\n';
     }
 }
