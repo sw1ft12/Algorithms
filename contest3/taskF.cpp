7755ab2f31b9a9d4e3eb80451a2b6d7caa0198c5
Daniil <99120105+sw1ft12@users.noreply.github.com>
2022-12-19 15:43:38 +0300

    Update taskF.cpp

--- contest3/taskF.cpp
+++ contest3/taskF.cpp
@@ -3,14 +3,26 @@
 #include <algorithm>
 
 
-using pll = std::pair<long long, long long>;
+struct Point {
+    long long x, y;
+    bool operator>(Point other) {
+        if (x == other.x) {
+            return y > other.y;
+        }
+        return x > other.x;
+    }
+};
 
+std::istream& operator>>(std::istream& in, Point &p) {
+    in >> p.x >> p.y;
+    return in;
+}
 
-long long cross_prod(const pll& a, const pll& b, const pll& c, const pll& d) {
-    long long x = b.first - a.first;
-    long long y = b.second - a.second;
-    long long x_ = d.first - c.first;
-    long long y_ = d.second - c.second;
+long long cross_prod(Point a, Point b, Point c, Point d) {
+    long long x = b.x - a.x;
+    long long y = b.y - a.y;
+    long long x_ = d.x - c.x;
+    long long y_ = d.y - c.y;
     long long prod = (x * y_) - (y * x_);
     if (prod == 0) {
         return 0;
@@ -20,44 +32,36 @@ long long cross_prod(const pll& a, const pll& b, const pll& c, const pll& d) {
 
 
 struct cmp {
-    pll start;
-    cmp(const pll &start) : start(start){}
-    bool operator()(const pll& a, const pll& b) {
+    Point start;
+    cmp(Point start) : start(start) {}
+    bool operator()(Point a, Point b) {
         long long sign = cross_prod(start, a, start, b);
         if (sign == 0) {
-            long long dist1 = (a.first - start.first) * (a.first - start.first) + (a.second - start.second) * (a.second - start.second);
-            long long dist2 = (b.first - start.first) * (b.first - start.first) + (b.second - start.second) * (b.second - start.second);
+            long long dist1 = (a.x - start.x) * (a.x - start.x) + (a.y - start.y) * (a.y - start.y);
+            long long dist2 = (b.x - start.x) * (b.x - start.x) + (b.y - start.y) * (b.y - start.y);
             return dist1 < dist2;
         }
         return sign > 0;
     }
 };
 
-
-int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cout.tie(0);
-    std::cin.tie(0);
-    int n;
-    std::cin >> n;
-    std::vector<pll> point(n);
-    pll start = { INT_MAX, INT_MAX };
+std::vector<Point> findConvexHull(std::vector<Point>& points) {
+    size_t n = points.size();
+    Point start = { INT_MAX, INT_MAX };
     int index = 0;
     for (size_t i = 0; i < n; ++i) {
-        std::cin >> point[i].first >> point[i].second;
-        if (start > point[i]) {
+        if (start > points[i]) {
             index = i;
-            start = point[i];
+            start = points[i];
         }
     }
-
-    std::swap(point[0], point[index]);
-    std::sort(point.begin() + 1, point.end(), cmp(point[0]));
-    std::vector<pll> hull;
+    std::swap(points[0], points[index]);
+    std::sort(points.begin() + 1, points.end(), cmp(points[0]));
+    std::vector<Point> hull;
     hull.push_back(start);
     for (size_t i = 1; i < n; ++i) {
         while (hull.size() >= 2) {
-            long long sign = cross_prod(hull[hull.size() - 2], hull.back(), hull.back(), point[i]);
+            long long sign = cross_prod(hull[hull.size() - 2], hull.back(), hull.back(), points[i]);
             if (sign <= 0) {
                 hull.pop_back();
             }
@@ -65,11 +69,24 @@ int main() {
                 break;
             }
         }
-        hull.push_back(point[i]);
+        hull.push_back(points[i]);
     }
+    return hull;
+}
 
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(0);
+    std::cin.tie(0);
+    int n;
+    std::cin >> n;
+    std::vector<Point> points(n);
+    for (size_t i = 0; i < n; ++i) {
+        std::cin >> points[i];
+    }
+    std::vector<Point> hull = findConvexHull(points);
     std::cout << hull.size() << '\n';
     for (const auto& p : hull) {
-        std::cout << p.first << ' ' << p.second << '\n';
+        std::cout << p.x << ' ' << p.y << '\n';
     }
 }
